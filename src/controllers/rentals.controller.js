aef751c11074d76fd6d7164e8b8d80dfe8e0994c
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-12-13 10:28:05 -0300

    get rentals and post rentals --> OK

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -7,30 +7,52 @@ import dayjs from "dayjs";
 //     daysRented: 3
 //   }
 
-export async function postrentalcontroller(req, res) {
+async function postrentalcontroller(req, res) {
   const { customerId, gameId, daysRented } = req.body;
 
   try {
-    const price = await connection.query(`SELECT "games"."pricePerDay" FROM games WHERE "games".id = $1`, [gameId]);
+    const price = await connection.query(`SELECT "pricePerDay" FROM games WHERE id=$1;`, [gameId])
     const originalPrice = price.rows[0].pricePerDay * daysRented;
     const rental = await connection.query(
       `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
       VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
       [customerId, gameId, dayjs().format("YYYY-MM-DD"), daysRented, null, originalPrice, null] 
     );
-    return res.send(rental.rows[0]);
+    return res.status(201).send(rental.rows[0]);
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
   }
 }
 
-export async function getrentalscontroller(req, res) {
-  try {
-    const rentals = await connection.query(`SELECT * FROM rentals WHERE "userId" = $1`, [req.user.id]);
-    return res.send(rentals.rows);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(500);
+async function getrentalscontroller(req, res) {
+  const { customerId } = req.query;
+    const { gameId } = req.query;
+
+    try {
+        if (customerId && !gameId) {
+            const rentals = await connection.query(`SELECT * FROM rentals WHERE "customerId" = $1`, [customerId]);
+            if (rentals.rows.length === 0) {
+                return res.status(404).send("Customer not found");
+            }
+            return res.send(rentals.rows);
+        }
+        if (gameId && !customerId) {
+            const rentals = await connection.query(`SELECT * FROM rentals WHERE "gameId" = $1`, [gameId]);
+            if (rentals.rows.length === 0) {
+              return res.status(404).send("Game not found");
+          }
+            return res.send(rentals.rows);
+        }
+       if (!customerId && !gameId) {
+            const rentals = await connection.query(`SELECT * FROM rentals`);
+            return res.send(rentals.rows);
+        }
+        return res.sendStatus(400);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
   }
 }
+
+export { postrentalcontroller, getrentalscontroller };
--- src/routes/rentals.route.js
+++ src/routes/rentals.route.js
@@ -5,7 +5,6 @@ import { rentalsmiddleware } from '../middlewares/rentals.middleware.js';
 const rentalsrouter = express.Router();
 
 rentalsrouter.post('/rentals', rentalsmiddleware, postrentalcontroller);
-
-// rentalsrouter.get('/rentals', controller);
+rentalsrouter.get('/rentals/', getrentalscontroller);
 
 export default rentalsrouter;
